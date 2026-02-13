ad19a6cbdf1342f6145032ab18987cd1b7d236e6
ZSzabolcs <zelenaksz@kkszki.hu>
2026-02-13 08:13:27 +0100

    További cuccok

---
+++ emil/Models/DTOs/SendMailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace emil.Models.DTOs
+{
+    public class SendMailDTO
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
--- emil/obj/Debug/net8.0/emil.AssemblyInfo.cs
+++ emil/obj/Debug/net8.0/emil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("emil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d634bfc245d8a974a5a921c8f3e1f52e35e521")]
 [assembly: System.Reflection.AssemblyProductAttribute("emil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("emil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
