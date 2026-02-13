f1dda3b65eef42c1290da000bd90ab66c96a4b05
ZSzabolcs <zelenaksz@kkszki.hu>
2026-02-13 08:36:46 +0100

    izé

--- emil/Services/GoogleMail.cs
+++ emil/Services/GoogleMail.cs
@@ -1,12 +1,25 @@
 ﻿using emil.Models.DTOs;
 using emil.Services.IMail;
+using MimeKit;
+using MimeKit.Text;
 namespace emil.Services
 {
     public class GoogleMail : ISendMail
     {
+        private readonly IConfiguration configuration;
+
+        public GoogleMail(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void Send(SendMailDTO sendMailDTO)
         {
-            throw new NotImplementedException();
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(configuration.GetSection("EmailSettings:EmailUserName").Value));
+            email.To.Add(MailboxAddress.Parse(sendMailDTO.To));
+            email.Subject = sendMailDTO.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = sendMailDTO.Body };
         }
     }
 }
